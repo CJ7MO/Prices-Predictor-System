69246fbc4ccdc7eb423c28931a9f050312a46823
cj7mo <cj7mo@outlook.com>
2024-09-30 13:51:59 -0500

    model building added

--- pipe/training_pipeline.py
+++ pipe/training_pipeline.py
@@ -3,7 +3,7 @@ from steps.handle_missing_values_step import handle_missing_values_step
 from steps.feature_engineering_step import feature_engineering_step
 from steps.outlier_detection_step import outlier_detection_step
 from steps.data_splitter_step import data_splitter_step
-
+from steps.model_building_step import model_building_step
 
 from zenml import Model, pipeline
 
@@ -35,7 +35,11 @@ def ml_pipeline():
     
     # Data splitting step
     X_train, X_test, y_train, y_test = data_splitter_step(clean_data, target_column="SalePrice")
-    return clean_data
+
+    # Model Building Step
+    model = model_building_step(X_train=X_train, y_train=y_train)
+    
+    return model
 
 if __name__ == "__main__":
     # Running the pipeline
